0a4294d5ae201b81dac52724e000641e580a25c1
Antony Lemos <djalma.rodrigues@luizalabs.com>
2022-11-13 11:49:01 -0300

    refactor: separate shared methods

---
+++ src/shared/HandleInput.java
@@ -0,0 +1,24 @@
+package shared;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Utility class to handle input values
+ * @author <a href="mailto:lemosantony@gmail.com">Antony Lemos</a>
+ */
+public class HandleInput {
+    /**
+     * Reads the number of iteractions to calculate the euler number
+     * @return Number of iteractions
+     */
+    public static final int readNumIteractions() throws InputMismatchException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of iterations: ");
+        int numIteractions = scanner.nextInt();
+
+        scanner.close();
+        return numIteractions;
+    }
+}
